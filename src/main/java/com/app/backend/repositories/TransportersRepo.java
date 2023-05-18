50eaa9de9832a4d9d40b45e9d54edff93d3a0569
zeljko-janjic <zeljkojanjic99@gmail.com>
2023-05-18 19:20:50 +0200

    prevoznik i linije features

--- src/main/java/com/app/backend/repositories/TransportersRepo.java
+++ src/main/java/com/app/backend/repositories/TransportersRepo.java
@@ -1,14 +1,32 @@
 package com.app.backend.repositories;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import lib.etickets.transporter.Transporter;
+import lib.etickets.transporter.route.Route;
 
 public class TransportersRepo {
-    List<Transporter> transportersDb = new ArrayList<>();
+    //for testing purposes
+    List<Transporter> transportersDb = Arrays.asList(new Transporter("Prevoznik1", "1",null),
+            new Transporter("Prevoznik2", "2",null),
+            new Transporter("Prevoznik3", "3",null));
 
     public List<Transporter> getAll(){
         return transportersDb;
+    }
+
+    public Transporter getTransporterByID(String id){
+        return transportersDb.get(0);
+    }
+
+    public void deleteTransporterById(String id){
+
+        //remove from db
+    }
+
+    public List<Route> getAllRoutesByTransporterID(String id){
+        return transportersDb.get(0).getRoutes();
     }
 }
